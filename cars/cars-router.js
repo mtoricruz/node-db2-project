16538ed448a358e67de6b0525dc8bcca9b853ea7
toricruzmendiola <mtoricruz@gmail.com>
2020-06-09 14:19:32 -0700

    completed 2 stretch: knex seeds and UPDATE/DELETE endpoints

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -17,7 +17,7 @@ router.post('/', (req, res) => {
         console.log("POST / error", err)
         res.status(500).json({ message: err.message });
     })
-})
+});
 
 // R =============================
 
@@ -29,6 +29,49 @@ router.get('/', (req, res) => {
     .catch(err => {
         res.status(500).json({ message: 'Failed to retrieve cars' });
     })
+});
+
+// U =============================
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    db('cars')
+        .where({ id })
+        .update(changes)
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json({ message: "car info updated!" })
+            } else {
+                res.status(404).json({ message: "no records found" })
+            }
+        })
+        .catch(err => {
+            console.log("PUT / error", err)
+            res.status(500).json({ message: err.message })
+        })
+})
+
+// D =============================
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('cars')
+        .where({ id })
+        .del()
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: "Car deleted" })
+            } else {
+                res.status(404).json({ message: "Car not found"})
+            }
+        })
+        .catch(err => {
+            console.log("DELETE / error", err)
+            res.status(500).json({ message: err.message })
+        })
 })
 
 module.exports = router;
